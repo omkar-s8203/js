0c472829c046e3480bba2b1ae5f7f8387596753f
omkar2020o <omkar876742@gmail.com>
2025-01-10 22:11:40 +0530

    update script reference and add string methods demonstration

---
+++ js part2/stringMethods_withArguments.js
@@ -0,0 +1,19 @@
+/*
+String methods with Arguments
+Argurmrnts is a some value that we pass to the methods
+
+format
+SringName.method(arg)
+*/
+
+/*  
+ondexOf 
+returns the first inex of occurrence of some value in string. or give -1 if not found.
+*/
+
+let str = "ILOveCoading"
+
+console.log(str.indexOf("LOve"));
+console.log(str.indexOf("ve"));
+console.log(str.indexOf("e"));
+console.log(str.indexOf("z"));
