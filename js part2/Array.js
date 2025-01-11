00d3ef2ece39b63240e669081a4a1f9ce43046b1
omkar2020o <omkar876742@gmail.com>
2025-01-11 12:41:14 +0530

    update script reference to Array.js and add array examples

---
+++ js part2/Array.js
@@ -0,0 +1,29 @@
+/*
+Array (data structure)
+Linerar collection of things
+
+let students = ["omkar", "jaysh", "atharva", "veadnt"];
+student[0]; //omkar
+*/
+
+
+let student = ["omkar", "aman", ];
+console.log(student[0]);
+console.log(student[1]);
+
+/* 
+VIsualizing Array
+*/
+
+let num = [2,5,8,69,89,78];
+
+let marks = [54,46,87,,78,48]
+let names = ["omkar" ,"diksha", "jayesh"];
+
+let info = ["omkar", 20, 1587.6]
+
+//empty
+let newArr= [];
+let emptyarray = [];
+
+console.log(marks.length);
