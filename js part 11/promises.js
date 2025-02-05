6e66924b37f045622ee634667d381d6e647222ee
omkar suryawanshi <omkar876742@gmail.com>
2025-02-05 20:38:28 +0530

    refactor promises.js to use Promise object for saveDB function; remove callback implementation and enhance error handling

--- js part 11/promises.js
+++ js part 11/promises.js
@@ -1,28 +1,51 @@
 /**
  * Promises 
- * the promise object represent the eventual completion (or failure) of an asynchronous operatio and its resulting value
+ * the promise object represent the eventual completion 
+ * (or failure) of an asynchronous operatio and its resulting value
  */
 
-function saveDB(data, succes, failure) {
-    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+// function saveDB(data, succes, failure) {
+//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
 
-    if (internetSpeed > 4) {
-        succes();
-    } else {
-        failure();
+//     if (internetSpeed > 4) {
+//         succes();
+//     } else {
+//         failure();
+//     }
+// }
+
+// saveDB(
+//     "Omkar",
+//     () => {
+//         console.log("ypur data succesfuylly saved");
+//         saveDB("hello world", () => {
+//             console.log("success2: data2 saved");
+//         }, () => {
+//             console.log("failure2: Weak connection");
+//         });
+//         saveDB("hello world", () => {
+//             console.log("success2: data2 saved");
+//         }, () => {
+//             console.log("failure2: Weak connection");
+//         });
+//     },
+//     () => {
+//         console.log("weak connection try again with good connection");
+//     });
+
+
+
+    function saveDB(data) {
+       
+         return new Promise((resolve, reject) =>{
+            let internetSpeed = Math.floor(Math.random() * 10) + 1;
+
+            if(internetSpeed>4){
+                resolve("success: data was saved");
+            }else{
+                reject("fail: something occur wrong ");
+            }
+         })
     }
-}
 
-saveDB(
-    "Omkar",
-    () => {
-        console.log("ypur data succesfuylly saved");
-        saveDB("hello world", ()=>{
-            console.log("success2: data2 saved");
-        },()=>{
-            console.log("failure2: Weak connection");
-        });
-    },
-    () => {
-        console.log("weak connection try again with good connection");
-    });
+    // saveDB("omkar suryawasnshi");
